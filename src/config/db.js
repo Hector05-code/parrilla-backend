cf638e4b56d509eeecd50f64da8599cc93adba32
Hector Francoç <hefbmy@gmail.com>
2026-06-09 20:40:36 -0400

    Agregar puerto DB

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,12 @@
-require('dotenv').config(); // ← AGREGÁ ESTA LÍNEA
-
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host:     process.env.DB_HOST,
   user:     process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port:     process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
